ead19306edabe5b31c7e48573dacc7a07f73f92a
v2 <mustafa_mahmodm@yahoo.com>
2025-12-15 21:44:18 +0200

    web

--- docs/web/flutter_service_worker.js
+++ docs/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0afba2cf3a341c136633146022b9a6ff",
+const RESOURCES = {"flutter_bootstrap.js": "abb71548532e3b279252b305d2a4a919",
 "version.json": "8721656392400567c036b38cb297f715",
 "splash/img/light-2x.png": "35d5db5b06e23e93aac53a45cb17c0f3",
 "splash/img/dark-4x.png": "b16ba354a9fce1b9f6f0fc3acc812d23",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "0afba2cf3a341c136633146022b9a6ff",
 "favicon.ico": "056bc06f94553a175b39f031166da80a",
 "index.html": "c72525c86a18b1c27dbb5d8cdc449199",
 "/": "c72525c86a18b1c27dbb5d8cdc449199",
-"main.dart.js": "e325294813b608ea749e5f7d16abe768",
+"main.dart.js": "dd3d0eaad4a2228e7aeca0c540a55e1f",
 ".well-known/apple-app-site-association": "faa53fd65cfcbd726567f42927425bd0",
 ".well-known/assetlinks.json": "1ab6e1f17e6eaf55d18a611360c0c2b8",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
--- lib/shared/functions/deeplink_helper.dart
+++ lib/shared/functions/deeplink_helper.dart
@@ -1,122 +1,122 @@
-// // import 'dart:developer';
+// import 'dart:developer';
 
-// // ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously
 
-// import 'package:app_links/app_links.dart';
-// import 'package:domandito/core/utils/shared_prefrences.dart';
-// import 'package:domandito/core/utils/utils.dart';
-// import 'package:domandito/main.dart';
-// import 'package:domandito/modules/profile/view/profile_screen.dart';
-// import 'package:domandito/modules/question/views/question_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:app_links/app_links.dart';
+import 'package:domandito/core/utils/shared_prefrences.dart';
+import 'package:domandito/core/utils/utils.dart';
+import 'package:domandito/main.dart';
+import 'package:domandito/modules/profile/view/profile_screen.dart';
+import 'package:domandito/modules/question/views/question_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
-// class DeepLinkHelper {
-//   final AppLinks _appLinks = AppLinks();
-//   bool _handledInitialLink = false;
-//   Uri? _lastHandledUri;
+class DeepLinkHelper {
+  final AppLinks _appLinks = AppLinks();
+  bool _handledInitialLink = false;
+  Uri? _lastHandledUri;
 
-//   void setupDeepLinkHandler({required BuildContext context}) async {
-//     // Stream listener for real-time deep links
-//     _appLinks.uriLinkStream.listen((Uri? uri) async {
-//       if (_shouldHandleUri(uri)) {
-//         // log('Deep link received (stream): $uri');
+  void setupDeepLinkHandler({required BuildContext context}) async {
+    // Stream listener for real-time deep links
+    _appLinks.uriLinkStream.listen((Uri? uri) async {
+      if (_shouldHandleUri(uri)) {
+        // log('Deep link received (stream): $uri');
 
-//         bool isBlocked = await checkIsBlocked();
+        bool isBlocked = await checkIsBlocked();
 
-//         if (!isBlocked) {
-//           _handleDeepLink(uri: uri!, context: context);
-//         }
-//       }
-//     });
+        if (!isBlocked) {
+          _handleDeepLink(uri: uri!, context: context);
+        }
+      }
+    });
 
-//     // Handle initial link
-//     if (!_handledInitialLink) {
-//       final Uri? initialLink = await _appLinks.getInitialLink();
-//       if (_shouldHandleUri(initialLink)) {
-//         // log('Initial deep link: $initialLink');
-//         _handledInitialLink = true;
-//         bool isBlocked = await checkIsBlocked();
+    // Handle initial link
+    if (!_handledInitialLink) {
+      final Uri? initialLink = await _appLinks.getInitialLink();
+      if (_shouldHandleUri(initialLink)) {
+        // log('Initial deep link: $initialLink');
+        _handledInitialLink = true;
+        bool isBlocked = await checkIsBlocked();
 
-//         if (!isBlocked) {
-//           _handleDeepLink(uri: initialLink!, context: context);
-//         }
-//       }
-//     }
-//   }
+        if (!isBlocked) {
+          _handleDeepLink(uri: initialLink!, context: context);
+        }
+      }
+    }
+  }
 
-//   bool _shouldHandleUri(Uri? uri) {
-//     if (uri == null) return false;
+  bool _shouldHandleUri(Uri? uri) {
+    if (uri == null) return false;
 
-//     // Block if it's exactly the same as the last one and it happened very recently
-//     if (uri == _lastHandledUri) return false;
+    // Block if it's exactly the same as the last one and it happened very recently
+    if (uri == _lastHandledUri) return false;
 
-//     _lastHandledUri = uri;
+    _lastHandledUri = uri;
 
-//     // Reset after a short delay to allow re-handling later
-//     Future.delayed(Duration(seconds: 2), () {
-//       _lastHandledUri = null;
-//     });
+    // Reset after a short delay to allow re-handling later
+    Future.delayed(Duration(seconds: 2), () {
+      _lastHandledUri = null;
+    });
 
-//     return true;
-//   }
+    return true;
+  }
 
-//   void _handleDeepLink({
-//     required Uri uri,
-//     required BuildContext context,
-//   }) async {
-//     // Handle deep link
-//     // log('Deep link handled: $uri');
+  void _handleDeepLink({
+    required Uri uri,
+    required BuildContext context,
+  }) async {
+    // Handle deep link
+    // log('Deep link handled: $uri');
 
-//     if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'q') {
-//       final questionId = uri.pathSegments.last;
-//       // log('Product ID: $productId');
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'q') {
+      final questionId = uri.pathSegments.last;
+      // log('Product ID: $productId');
 
-//       // Delay navigation slightly to ensure context is ready
+      // Delay navigation slightly to ensure context is ready
 
-//       Future.delayed(Duration(milliseconds: 100), () async {
-//         try {
-//           final res = await getQuestionData(questionId: questionId);
-//           if (res != null) {
-//             final q = res;
+      Future.delayed(Duration(milliseconds: 100), () async {
+        try {
+          final res = await getQuestionData(questionId: questionId);
+          if (res != null) {
+            final q = res;
 
-//             pushScreen(
-//               navigatorKey.currentState!.context,
-//               screen: QuestionScreen(
-//                 isVerified: false,
-//                 question: q,
-//                 receiverImage: q.receiver.image,
-//                 onBack: (s) {},
-//                 currentProfileUserId: MySharedPreferences.userId,
-//               ),
-//             );
-//           } else {
-//             // debugPrint("No restaurant found with id: $resId");
-//           }
-//         } catch (e) {
-//           // debugPrint("Error loading restaurant: $e");
-//         }
-//       });
-//     }
-//     if (uri.pathSegments.length == 1 && uri.pathSegments.first != 'q') {
-//       final userUserName = uri.pathSegments.last;
-//       // log('Product ID: $productId');
+            pushScreen(
+              navigatorKey.currentState!.context,
+              screen: QuestionScreen(
+                isVerified: false,
+                question: q,
+                receiverImage: q.receiver.image,
+                onBack: (s) {},
+                currentProfileUserId: MySharedPreferences.userId,
+              ),
+            );
+          } else {
+            // debugPrint("No restaurant found with id: $resId");
+          }
+        } catch (e) {
+          // debugPrint("Error loading restaurant: $e");
+        }
+      });
+    }
+    if ((uri.pathSegments.length == 1 && uri.pathSegments.first != '/' )|| uri.pathSegments.length == 1 && uri.pathSegments.first != '#/' ) {
+      final userUserName = uri.pathSegments.last;
+      // log('Product ID: $productId');
 
-//       // Delay navigation slightly to ensure context is ready
-//       if (userUserName == MySharedPreferences.userUserName) {
-//         return;
-//       }
-//       final res = await getProfileByUserNameForDeepLink(
-//         userUserName: userUserName,
-//       );
-//       if (res != null) {
-//         Future.delayed(Duration(milliseconds: 100), () async {
-//           pushScreen(
-//             navigatorKey.currentState!.context,
-//             screen: ProfileScreen(userId: res.id, userUserName: ''),
-//           );
-//         });
-//       }
-//     }
-//   }
-// }
+      // Delay navigation slightly to ensure context is ready
+      if (userUserName == MySharedPreferences.userUserName) {
+        return;
+      }
+      final res = await getProfileByUserNameForDeepLink(
+        userUserName: userUserName,
+      );
+      if (res != null) {
+        Future.delayed(Duration(milliseconds: 100), () async {
+          pushScreen(
+            navigatorKey.currentState!.context,
+            screen: ProfileScreen(userId: res.id, userUserName: ''),
+          );
+        });
+      }
+    }
+  }
+}
