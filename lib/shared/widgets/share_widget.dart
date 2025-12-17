97b0da3ef77423fc9e47e53d03e59ff9bc13d4fd
v2 <mustafa_mahmodm@yahoo.com>
2025-12-17 19:17:53 +0200

    share

--- lib/shared/widgets/share_widget.dart
+++ lib/shared/widgets/share_widget.dart
@@ -1,7 +1,9 @@
 import 'package:domandito/core/constants/app_constants.dart';
 import 'package:domandito/core/constants/app_icons.dart';
+import 'package:domandito/core/services/share_service.dart';
 import 'package:domandito/shared/services/share_service.dart';
 import 'package:domandito/shared/style/app_colors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
 import 'package:svg_flutter/svg.dart';
@@ -32,14 +34,22 @@ class _ShareWidgetState extends State<ShareWidget> {
         backgroundColor: WidgetStatePropertyAll(AppColors.white),
       ),
       onPressed: () async {
-        if (widget.userUserName.isNotEmpty) {
-          AppConstance().showLoading( context);
-          await ShareService.shareUserCard(
-            userImage: widget.userImage,
-            username: widget.userUserName,
-          ).then(
-            (value) => Loader.hide(),
-          );
+        if (kIsWeb) {
+          if (widget.userUserName.isNotEmpty) {
+            await ShareService1.shareContent(
+              data:
+                  'Ask me at ${AppConstance.shareLink}#/${widget.userUserName}',
+              context: context,
+            );
+          }
+        } else {
+          if (widget.userUserName.isNotEmpty) {
+            AppConstance().showLoading(context);
+            await ShareService.shareUserCard(
+              userImage: widget.userImage,
+              username: widget.userUserName,
+            ).then((value) => Loader.hide());
+          }
         }
       },
 
