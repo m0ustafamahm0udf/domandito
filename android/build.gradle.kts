0d4be96735ffacf6b5a45b5dd1e981662e171213
v2 <mustafa_mahmodm@yahoo.com>
2025-12-24 21:22:56 +0200

    feat: Generate Kotlin compiler session and build problems report files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,30 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    val project = this
+    val fixAction = {
+         val android = project.extensions.findByName("android")
+         if (android != null) {
+            try {
+                val namespaceMethod = android::class.java.getMethod("getNamespace")
+                val namespace = namespaceMethod.invoke(android)
+                if (namespace == null) {
+                     val setNamespaceMethod = android::class.java.getMethod("setNamespace", String::class.java)
+                     setNamespaceMethod.invoke(android, project.group.toString())
+                }
+            } catch (e: Exception) {
+                // Ignore if method not found
+            }
+         }
+    }
+    
+    if (project.state.executed) {
+        fixAction()
+    } else {
+        project.afterEvaluate {
+            fixAction()
+        }
+    }
+}
