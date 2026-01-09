209ee689c14e9b4998c1825bd30688e2b4db29a0
v2 <mustafa_mahmodm@yahoo.com>
2026-01-09 23:10:43 +0200

    web

--- docs/web/flutter_bootstrap.js
+++ docs/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "500961169"
+    serviceWorkerVersion: "1251279538"
   }
 });
