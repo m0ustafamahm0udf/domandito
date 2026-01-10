d527319f93d67ddf616e49fb78db373cf4e5fbb9
v2 <mustafa_mahmodm@yahoo.com>
2026-01-10 13:56:11 +0200

    1.0.7+37

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -72,7 +72,7 @@ class AppConstance {
   }
 
   static const double textFieldH = 18;
-  static const String appVersion = "1.0.6+36";
+  static const String appVersion = "1.0.7+37";
   static const String question = "question";
   static const String like = "like";
   static const String follow = "follow";
